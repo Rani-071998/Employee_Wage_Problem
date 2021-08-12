7d31ed765ac6eb40646fd1c0310df1ed43fcc9d0
Rani-071998 <rhendre.07@gmail.com>
2021-08-12 21:01:31 +0530

    ADDED employee wages for month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,16 +2,20 @@
  echo " Welcome to Employee Wage computation program  on master banch"
 is_present_full_time=1
 is_present_half_time=2
-randomcheck=$((RANDOM%2))
 empRateperHr=20
-if [ $randomcheck -eq $is_present_full_time ] 
-then
-     empHr=8 
-elif [ $randomcheck -eq $is_present_half_time ] 
-then 
-      empHr=4 
-else
-      empHr=o 
-fi
+for ((day=1;day<=20;day++))
+do 
+ randomcheck=$((RANDOM%2))
+
+case $randomcheck in
+     $is_present_full_time)
+     empHr=8 ;;
+    $is_present_half_time )
+      empHr=4 ;;
+    *)
+      empHr=o ;;
+esac
 salary=$(( empRateperHr * empHr ))
+done
 echo $salary
+
