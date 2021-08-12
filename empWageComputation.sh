184670f8e16e16927b34fc3a313a9fbada54c43a
Rani-071998 <rhendre.07@gmail.com>
2021-08-12 18:50:04 +0530

    ADDED employee wage salary

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,9 +2,13 @@
  echo " Welcome to Employee Wage computation program  on master banch"
 ispresent=0
 randomcheck=$((RANDOM%2))
+empRateperHr=20
+workHr=8
 if [  $randomcheck -eq  $ispresent ]
 then 
  echo "employe present"
 else
 echo "employee not present"
 fi
+salary=$(( empRateperHr * wokHr ))
+echo $salary
