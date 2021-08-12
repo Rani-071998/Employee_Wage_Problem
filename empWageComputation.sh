482d1846b532ccc879ff8241f4cd1d4bdce83786
Rani-071998 <rhendre.07@gmail.com>
2021-08-12 20:36:13 +0530

    ADDED employee switch case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,18 +2,16 @@
  echo " Welcome to Employee Wage computation program  on master banch"
 is_present_full_time=1
 is_present_half_time=2
-randomcheck=$((RANDOM%3))
+randomcheck=$((RANDOM%2))
 empRateperHr=20
+case $randomcheck in
+  $is_present_full_time) 
+     empHr=8 ;;
 
-if [  $randomcheck -eq  $is_present_full_time ]
-then 
-  empHr=8
-
-elif [ $randomcheck -eq $is_present_half_time ]
-then 
-    empHr=4
-else
-    empHr=0
-fi
+   $is_present_half_time) 
+      empHr=4 ;;
+  *) 
+      empHr=o ;;
+esac
 salary=$(( empRateperHr * empHr ))
 echo $salary
