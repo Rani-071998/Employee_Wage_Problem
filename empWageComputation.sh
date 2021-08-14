4187ed3619cda5b30b0a32e36bbb53a5cd093f4c
Rani-071998 <rhendre.07@gmail.com>
2021-08-14 17:12:09 +0530

    ADDED employee store daily wage along sithtotal

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,5 +1,8 @@
 #! /bin/bash -x
  echo " Welcome to Employee Wage computation program  on master banch"
+# array declaration
+declare -a dailyWage
+# constant varialbles
 IS_PRESENT_FULL_TIME=1
 IS_PRESENT_HALF-TIME=2
 EMP_RATE_PER_HR=20
@@ -16,23 +19,35 @@ function getworkHr() {
         $IS_PRESENT_HALF_TIME)
         empHr=4;;
         *)
-        empHr=0;;
+          empHr=0;;
 esac
         echo $empHr
+}
+function getWage(){
+        local empHr=$1
+        echo $(( empHr * EMP_RATE_PER_HR ))
+
 }
 
 while [ $day -le $MAX_WORKING_DAY ] && [ $totalworkingHr -lt $MAX_WORK_HR ]
 do
+        # variable
         empCheck=$(( RANDOM%3 ))
+
         #CALLING FUNCTION
         empHr="$( getworkHr $empCheck )"
-        totalworkingHr=$(( totalworkingHr + empHr ))
-        salary=$(( empHr * EMP_RATE_PER_HR ))
-        echo sal:-$salary
-        ((day++))
+ totalworkingHr=$(( totalworkingHr + empHr ))
+       # salary=$(( empHr * EMP_RATE_PER_HR ))
+
+        dailyWage[((day++))]=$( getWage $empHr)
+
 done
-totalsalary=$(( totalworkingHr * EMP_RATE_PER_HR ))
+totalsalary=$( getWage $totalworkingHr )
 echo "total:-$totalsalary"
+echo dailyWages:${dailyWage[*]}
+
+
+
 
 
 
