c1df68527ef04c4bb8929fb256eb23b0a020d8c0
Rani-071998 <rhendre.07@gmail.com>
2021-08-14 16:52:43 +0530

    ADDED employee wage with function

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,29 +1,39 @@
 #! /bin/bash -x
  echo " Welcome to Employee Wage computation program  on master banch"
-is_present_full_time=1
-is_present_half_time=2
-empRateperHr=20
+IS_PRESENT_FULL_TIME=1
+IS_PRESENT_HALF-TIME=2
+EMP_RATE_PER_HR=20
+MAX_WORKING_DAY=20
+MAX_WORK_HR=70
 totalworkingHr=0
-maxworkingdays=20
 day=1
-while [ $day -le $maxworkingdays ] 
-do 
- randomcheck=$((RANDOM%2))
-
-case $randomcheck in
-     $is_present_full_time)
-     empHr=8 ;;
-    $is_present_half_time )
-      empHr=4 ;;
-    *)
-      empHr=o ;;
+function getworkHr() {
+        local empCheck=$1
+        local empHr=0
+        case $empCheck in
+        $IS_PRESENT_FULL_TIME)
+        empHr=8;;
+        $IS_PRESENT_HALF_TIME)
+        empHr=4;;
+        *)
+        empHr=0;;
 esac
-totalworkonHr=$(( totalworkingHr + empHr ))
-salary=$(( empRateperHr * empHr ))
-echo "salary:"$salary
-((day++))
+        echo $empHr
+}
+
+while [ $day -le $MAX_WORKING_DAY ] && [ $totalworkingHr -lt $MAX_WORK_HR ]
+do
+        empCheck=$(( RANDOM%3 ))
+        #CALLING FUNCTION
+        empHr="$( getworkHr $empCheck )"
+        totalworkingHr=$(( totalworkingHr + empHr ))
+        salary=$(( empHr * EMP_RATE_PER_HR ))
+        echo sal:-$salary
+        ((day++))
 done
-totalsalary=$(( totalworkingHr * empRateperHr ))
-echo "Total salary:="$totalsalary
+totalsalary=$(( totalworkingHr * EMP_RATE_PER_HR ))
+echo "total:-$totalsalary"
+
+
 
 
